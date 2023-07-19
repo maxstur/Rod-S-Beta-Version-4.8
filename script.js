7d4c3a05581654266e7f6e9ce4a7c3d607e08f9d
Maximiliano Rodríguez <maarashu@gmail.com>
2023-07-19 20:28:21 -0300

    cargatm

---
+++ script.js
@@ -0,0 +1,15 @@
+var tm = new Tm (".myTm",{
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlide: true,
+    slidePerView: "auto",
+    coverFlowEffect: {
+        rotate:15,
+        stech:0,
+        depth:300,
+        modifier:1,
+        slideShadows: true,
+    },
+    loop:true,
+
+});
