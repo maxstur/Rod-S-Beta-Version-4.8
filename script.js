a684b911d90944c988427530191b406d297f3ad4
Maximiliano Rodríguez <maarashu@gmail.com>
2023-07-22 21:42:08 -0300

    navbar upload with hamburguer menu, src with details, pages added, content fot lern with begin, some more details

--- script.js
+++ script.js
@@ -11,5 +11,24 @@ var tm = new Tm (".myTm",{
         slideShadows: true,
     },
     loop:true,
-
 });
+
+const waCont = document.querySelector('.wa-cont');
+const waBottom = document.querySelector('.wa-bottom');
+
+function ajustarVisibilidadElemento() {
+
+    const scrollY = window.scrollY || window.pageYOffseet;
+
+    const posicionInicio = 850;
+    const posicionFin = 1850;
+
+    if (scrollY >= posicionInicio && scrollY <= posicionFin) {
+        waCont.style.display = 'block';
+    } else {
+        waCont.style.display = 'none';
+    }
+}
+window.addEventListener('scroll', ajustarVisibilidadElemento);
+ajustarVisibilidadElemento();
+
